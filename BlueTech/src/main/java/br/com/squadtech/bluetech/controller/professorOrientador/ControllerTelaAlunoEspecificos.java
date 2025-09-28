0229dabb51bd21fe3c68a376028dfd62d4cca1c1
guivalim <guilhermevalim05@gmail.com>
2025-09-28 19:52:49 -0300

    Chore(#PT.O02)
    Aqui foi adicionado um botão para passar para próxima tela que for ligada no projeto.

--- BlueTech/src/main/java/br/com/squadtech/bluetech/controller/professorOrientador/ControllerTelaAlunoEspecificos.java
+++ BlueTech/src/main/java/br/com/squadtech/bluetech/controller/professorOrientador/ControllerTelaAlunoEspecificos.java
@@ -1,17 +1,33 @@
 package br.com.squadtech.bluetech.controller.professorOrientador;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ControllerTelaAlunoEspecificos {
 
     @FXML
-    private MenuButton MenuButtonSemestre;
+    private ToggleButton ButtonProximaTela2;
 
     @FXML
-    private ToggleButton ToggleButonTodosTg;
+    void passarTelaIndefinida(ActionEvent event) throws IOException {
+        // o metodo abaixo é onde passa para proxima tela, vc só precisa por pra qual tela vai ali entre as aspas
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        Scene scene = new Scene(root);
 
+        // pega a janela atual
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
 
 
--- BlueTech/src/main/java/module-info.java
+++ BlueTech/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     exports br.com.squadtech.bluetech;
     exports br.com.squadtech.bluetech.controller.professorOrientador;
