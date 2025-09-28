928ae16c30f9118ea044de6f9325752b667a43c6
Giovanna Marques <giovanna.rodrigues9@fatec.sp.gov.br>
2025-09-28 20:48:56 -0300

    feat(#D.A01) Tela de enviar arquivo
    Adicionar tela do aluno para enviar o arquivo e o controller

---
+++ BlueTech/src/main/java/br/com/squadtech/bluetech/controller/aluno/TelaAlunoController.java
@@ -0,0 +1,18 @@
+package br.com.squadtech.bluetech.controller.aluno;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaAlunoController {
+
+        @FXML
+        private Button btn_estudante_enviarArquivo;
+
+        @FXML
+        void enviarArquivo(ActionEvent event) {
+
+        }
+    }
+
