8a506d62ec091f4144372de0c35ac3df4514cc6f
menegasso0 <menegasso00@gmail.com>
2025-09-28 19:45:15 -0300

    feat(#PT.TG02)
    Foi inserido ao código o protótipo da tela de visualização e o seu controller, de arquivos MD finais.

---
+++ BlueTech/src/main/java/br/com/squadtech/bluetech/controller/professorTG/VisualizadorTGController.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class VisualizadorTGController {
+
+
+    @FXML
+    private CheckBox cbx_professorTG_VisuAluno;
+
+    @FXML
+    private Button btn_professorTG_finalizar;
+
+
+    @FXML
+    void handleCheckboxAction(ActionEvent event) {
+        if (cbx_professorTG_VisuAluno.isSelected()) {
+            System.out.println("CheckBox 'TG João P.' foi MARCADO.");
+        } else {
+            System.out.println("CheckBox 'TG João P.' foi DESMARCADO.");
+        }
+    }
+
+
+    @FXML
+    void handleFinalizarButtonAction(ActionEvent event) {
+        System.out.println("O botão 'Finalizar' foi clicado.");
+
+    }
+
+
+    @FXML
+    void initialize() {
+        System.out.println("PrimaryController foi inicializado e está pronto.");
+
+        if (cbx_professorTG_VisuAluno.isSelected()) {
+            System.out.println("O CheckBox 'TG João P.' já começou marcado.");
+        }
+    }
+}
