b3e3c1c7b8504bd95afac653425e70ef20879a48
guivalim <guilhermevalim05@gmail.com>
2025-11-20 14:24:04 -0300

    feat(#U02): Editar Perfil Professor
    Criada a tela de editar perfil do professor.

---
+++ BlueTech/src/main/java/br/com/squadtech/bluetech/controller/professorOrientador/TelaPerfilProfessorController.java
@@ -0,0 +1,58 @@
+package br.com.squadtech.bluetech.controller.professorOrientador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class TelaPerfilProfessorController{
+
+    @FXML
+    private AnchorPane AncorPanePerfilProfessor;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private Button btnSelecionarFoto;
+
+    @FXML
+    private ImageView imgFoto;
+
+    @FXML
+    private TextField txtCargo;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtNovaSenha;
+
+    @FXML
+    private TextField txtSenhaAtual;
+
+    @FXML
+    void handleCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSalvar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSelecionarFoto(ActionEvent event) {
+
+    }
+
+}
